3485d39314a14b376854ba80bad3ffed322ef0fb
c110156228 <102135379+c110156228@users.noreply.github.com>
2022-04-06 17:31:57 +0800

    生肖

---
+++ 3.py
@@ -0,0 +1,26 @@
+#生肖
+year = int(input("請輸入西元年"))
+if (year-1911)%12 ==0:
+    print("豬")
+elif (year-1911)%12 ==1:
+    print("鼠")
+elif (year-1911)%12 ==2:
+    print("牛")
+elif (year-1911)%12 ==3:
+    print("虎")
+elif (year-1911)%12 ==4:
+    print("兔")
+elif (year-1911)%12 ==5:
+    print("龍")
+elif (year-1911)%12 ==6:
+    print("蛇")
+elif (year-1911)%12 ==7:
+    print("馬")
+elif (year-1911)%12 ==8:
+    print("羊")
+elif (year-1911)%12 ==9:
+    print("猴")
+elif (year-1911)%12 ==10:
+    print("雞")
+elif (year-1911)%12 ==11:
+    print("狗")
