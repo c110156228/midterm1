c8b19af3e1b7091281fd4e58376b9cefa9592a6a
c110156228 <102135379+c110156228@users.noreply.github.com>
2022-04-06 17:09:14 +0800

    提款機搜尋

---
+++ 22.py
@@ -0,0 +1,33 @@
+#提款機搜尋
+account = input("輸入帳號")
+password = input("輸入密碼")
+if account =='123':
+    if password =='456':
+        print(9000)
+    else:
+        print("error")
+if account =='456':
+    if password =='789':
+        print(5000)
+    else:
+        print("error")
+if account =='789':
+    if password =='888':
+        print(6000)
+    else:
+        print("error")
+if account =='336':
+    if password =='558':
+        print(10000)
+    else:
+        print("error")
+if account =='775':
+    if password =='666':
+        print(12000)
+    else:
+        print("error")
+if account =='566':
+    if password =='221':
+        print(7000)
+    else:
+        print("error")
