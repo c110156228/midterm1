8c182fe74a4cf90add0cda43bb188f7413df0af7
c110156228 <102135379+c110156228@users.noreply.github.com>
2022-05-31 12:06:12 +0800

    顏色

---
+++ 62.py
@@ -0,0 +1,17 @@
+#顏色
+dict = {"蘋果":"紅色", "香蕉":"黃色", "葡萄":"紫色", "藍莓":"藍色", "橘子":"橘色"}
+s = input("請輸入水果")
+if s == "蘋果":
+    print(dict["蘋果"])
+elif s == "香蕉":
+    print(dict["香蕉"])
+elif s == "葡萄":
+    print(dict["葡萄"])
+elif s == "藍莓":
+    print(dict["藍莓"])
+elif s == "橘子":
+    print(dict["橘子"])
+
+
+
+
